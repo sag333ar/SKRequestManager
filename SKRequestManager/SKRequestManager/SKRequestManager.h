e77e73cceeb51acf635d83b172f453362313fc6b
Sagar Kothari <skothari@altimetrik.com>
2016-12-28 19:03:37 +0530

    Added files for carthage

---
+++ SKRequestManager/SKRequestManager/SKRequestManager.h
@@ -0,0 +1,19 @@
+//
+//  SKRequestManager.h
+//  SKRequestManager
+//
+//  Created by Sagar on 12/28/16.
+//  Copyright © 2016 Sagar R. Kothari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKRequestManager.
+FOUNDATION_EXPORT double SKRequestManagerVersionNumber;
+
+//! Project version string for SKRequestManager.
+FOUNDATION_EXPORT const unsigned char SKRequestManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKRequestManager/PublicHeader.h>
+
+
